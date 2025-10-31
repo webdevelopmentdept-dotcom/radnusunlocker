f4a92fee38b0ab3c9af6829cd304a2d1e5dfb609
webdevelopmentdept-dotcom <webdevelopmentdept@gmail.com>
2025-10-31 18:41:33 +0530

    Deletion of helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // ✅ Bootstrap first
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
+
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css"; // ✅ Your custom styles after Bootstrap
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
+  <BrowserRouter>
     <App />
-  </HelmetProvider>
+  </BrowserRouter>
 );
